1f3a0f3cd9ef8bdbac0ba85405a5a10e6234d7ca
oakes <zsoakes@gmail.com>
2016-03-17 11:15:02 -0400

    Add routes

--- src/main/java/com/theironyard/services/UserRepository.java
+++ src/main/java/com/theironyard/services/UserRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.repository.CrudRepository;
  * Created by zach on 3/17/16.
  */
 public interface UserRepository extends CrudRepository<User, Integer> {
+    User findByName(String name);
 }
